d7375a4388d6d6667fedbf5a9ce8b7aa8d004a89
matheus.toniotti <matheus.toniotti@gmail.com>
2020-01-27 16:25:49 -0300

    arrumar configurações e controller da sala que estava como @RequestParam e deveria ser @PathVariable

--- back/src/main/java/br/com/hbsis/faculdade/controller/SalaController.java
+++ back/src/main/java/br/com/hbsis/faculdade/controller/SalaController.java
@@ -23,17 +23,17 @@ public class SalaController {
     }
 
     @PostMapping("/sala/update/{serie}/{letra}")
-    public SalaDTO update(@Valid @RequestBody SalaDTO salaDTO, @RequestParam("serie") Integer serie, @RequestParam("letra") String letra){
+    public SalaDTO update(@Valid @RequestBody SalaDTO salaDTO, @PathVariable("serie") Integer serie, @PathVariable("letra") String letra){
         return this.salaService.update(salaDTO, serie, letra);
     }
 
     @PostMapping("/sala/delete/{serie}/{letra}")
-    public SalaDTO delete(@RequestParam("serie") Integer serie, @RequestParam("letra") String letra){
+    public SalaDTO delete(@PathVariable("serie") Integer serie, @PathVariable("letra") String letra){
         return this.salaService.delete(serie, letra);
     }
 
     @GetMapping("/sala/{serie}/{letra}")
-    public SalaDTO getSala(@RequestParam("serie") Integer serie, @RequestParam("letra") String letra){
+    public SalaDTO getSala(@PathVariable("serie") Integer serie, @PathVariable("letra") String letra){
         return SalaDTO.of(this.salaService.findEntityByLetra(serie, letra));
     }
 }
