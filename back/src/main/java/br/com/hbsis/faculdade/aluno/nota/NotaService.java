8f5474e492d4bbe5ac12671078fc8ae29a6120dd
matheus.toniotti <matheus.toniotti@gmail.com>
2020-02-04 08:35:59 -0300

    adicionando sistem de notas.

---
+++ back/src/main/java/br/com/hbsis/faculdade/aluno/nota/NotaService.java
@@ -0,0 +1,56 @@
+package br.com.hbsis.faculdade.aluno.nota;
+
+import br.com.hbsis.faculdade.aluno.AlunoService;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.NoResultException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class NotaService {
+    private final INotaRespository notaRespository;
+    private final AlunoService alunoService;
+
+    public NotaService(INotaRespository notaRespository, AlunoService alunoService) {
+        this.notaRespository = notaRespository;
+        this.alunoService = alunoService;
+    }
+
+    public NotaDTO save(NotaDTO notaDTO){
+        Nota nota = new Nota();
+        nota.setAluno(this.alunoService.getEntityById(notaDTO.getIdAluno()));
+        nota.setDescricao(notaDTO.getDesc());
+        nota.setNota(notaDTO.getNota());
+
+        return NotaDTO.of(this.notaRespository.save(nota));
+    }
+
+    public NotaDTO update(NotaDTO notaDTO, Long idNota){
+        Optional<Nota> notaOptional = this.notaRespository.findById(idNota);
+
+        if(notaOptional.isPresent()){
+            Nota nota = notaOptional.get();
+            nota.setNota(notaDTO.getNota());
+            nota.setDescricao(notaDTO.getDesc());
+            nota.setAluno(this.alunoService.getEntityById(notaDTO.getIdAluno()));
+
+            return NotaDTO.of(nota);
+        }
+        throw new NoResultException("Nota não encontrada.");
+    }
+
+    public void delete(Long idNota){
+        this.notaRespository.deleteById(idNota);
+    }
+
+    public List<NotaDTO> getNotaByAluno(Long idAluno){
+        List<NotaDTO> notaDTOList = new ArrayList<>();
+        this.notaRespository.findByAluno(this.alunoService.getEntityById(idAluno)).forEach(nota -> notaDTOList.add(NotaDTO.of(nota)));
+
+        if(notaDTOList.isEmpty()){throw new NoResultException("Nenhuma nota encontrada para esse aluno.");}
+
+        return notaDTOList;
+    }
+}
---
+++ back/src/main/java/br/com/hbsis/faculdade/controller/NotaController.java
@@ -0,0 +1,37 @@
+package br.com.hbsis.faculdade.controller;
+
+import br.com.hbsis.faculdade.aluno.nota.NotaDTO;
+import br.com.hbsis.faculdade.aluno.nota.NotaService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class NotaController {
+    private final NotaService notaService;
+
+    public NotaController(NotaService notaService) {
+        this.notaService = notaService;
+    }
+
+    @PostMapping("/nota/save")
+    public NotaDTO save(@RequestBody NotaDTO notaDTO){
+        return this.notaService.save(notaDTO);
+    }
+
+    @DeleteMapping("/nota/delete/{idNota}")
+    public void delete(@PathVariable("idNota") Long idNota){
+        this.notaService.delete(idNota);
+    }
+
+    @PutMapping("/nota/update/{idNota}")
+    public NotaDTO update(@RequestBody NotaDTO notaDTO, @PathVariable("idNota") Long idNota){
+        return this.notaService.update(notaDTO, idNota);
+    }
+
+    @GetMapping("/nota/get/{idAluno}")
+    public List<NotaDTO> getNotasByAluno(@PathVariable("idAluno") Long idAluno){
+        return this.notaService.getNotaByAluno(idAluno);
+    }
+}
